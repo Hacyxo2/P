ac84eb875f09447b8556ec770ca7f0fda3650373
Hacyxo2 <83963272+Hacyxo2@users.noreply.github.com>
2021-09-03 21:52:28 +0900

    add

--- src/Screen.java
+++ src/Screen.java
@@ -2,42 +2,27 @@ import java.awt.Canvas;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+
+
 
 public class Screen extends Canvas implements KeyListener {
-
-	private int x1 = 0;
-	private int y1= 0;
-	private int x2 = 740;
-	private int y2 = 520;
-	private int x3 = 40;
-	private int y3 = 0;
 	
-	
-	private BufferedImage image;
-	private BufferedImage image1;
+	Car car[] = new Car[3];
 	private static final long serialVersionUID = 1L;
 	
 	public Screen() {
-		try {
-			image = ImageIO.read(new File("src/car.png"));
-			image1 = ImageIO.read(new File("src/car1.png"));
-			
-			addKeyListener(this);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		car[0] = new Car(0, 0, "src/car1.png");
+		car[1] = new Car(0, 50, "src/car.png");
+		car[2] = new Car(300, 500,"src/car2.png");
+		addKeyListener(this);
 	}
 	@Override
 	public void paint(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paint(g);
-		g.drawImage(image, x1, y1, this);
-		g.drawImage(image, x3, y3, this);
-		g.drawImage(image1, x2, y2, this);
+		g.drawImage(car[0].image, car[0].x, car[0].y, this);
+		g.drawImage(car[1].image, car[1].x, car[1].y, this);
+		g.drawImage(car[2].image, car[2].x, car[2].y, this);
 	}
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -56,40 +41,42 @@ public class Screen extends Canvas implements KeyListener {
 		switch(e.getKeyCode())
 		{
 		//1p
+		case 87: //UP
+			car[0].y -= gap;
+			car[1].y -= gap;
+			break;
+		case 83: //DOWN
+			car[0].y += gap;
+			car[1].y += gap;
+			break;
+		case 65: //LEFT
+			car[0].x -= gap;
+			car[1].x -= gap;
+			break;
+		case 68:  //RIGHT
+			car[0].x += gap;
+			car[1].x += gap;
+			break;
+		//2p		
 		case KeyEvent.VK_UP:
-			y1 -= gap;
-			y3 -= gap;
+			car[2].y -= gap;
 			break;
 		case KeyEvent.VK_DOWN:
-			y1 += gap;
-			y3 += gap;
+			car[2].y += gap;
 			break;
 		case KeyEvent.VK_LEFT:
-			x1 -= gap;
-			x3 -= gap;
+			car[2].x -= gap;
 			break;
 		case KeyEvent.VK_RIGHT:
-			x1 += gap;
-			x3 += gap;
-			break;
-		//2p
-		case 87:
-			y2 -= gap;
-			break;
-		case 83:
-			y2 += gap;
-			break;
-		case 65:
-			x2 -= gap;
-			break;
-		case 68:
-			x2 += gap;
+			car[2].x += gap;
 			break;
 		}
-		System.out.println(x1+", "+y1);
+		System.out.println("1P"+car[0].x+", "+"1P"+car[0].y+"\t"
+		+"2P"+car[2].x+", "+"2P"+car[2].y);
 		repaint();
 	}
-	
+}
+
 	
 
-}
+
